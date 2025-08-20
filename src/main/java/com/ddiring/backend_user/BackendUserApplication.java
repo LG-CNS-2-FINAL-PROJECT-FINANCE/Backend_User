597fefef46fc6772fbe2a980e6d635ee2a5184f6
dohy0103 <kdohy0103@gmail.com>
2025-08-20 12:50:50 +0900

    Update: 한국 시간대로 변경

--- src/main/java/com/ddiring/backend_user/BackendUserApplication.java
+++ src/main/java/com/ddiring/backend_user/BackendUserApplication.java
@@ -3,12 +3,14 @@ package com.ddiring.backend_user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableFeignClients
 public class BackendUserApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(BackendUserApplication.class, args);
     }
 }
