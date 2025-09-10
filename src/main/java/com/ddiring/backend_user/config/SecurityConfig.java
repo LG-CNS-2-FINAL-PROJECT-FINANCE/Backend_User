f57011d6fb7a16a5eeb1432c8a2b56c220d41def
dohy0103 <kdohy0103@gmail.com>
2025-09-10 20:05:19 +0900

    update: 0.7.3

--- src/main/java/com/ddiring/backend_user/config/SecurityConfig.java
+++ src/main/java/com/ddiring/backend_user/config/SecurityConfig.java
@@ -9,15 +9,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -34,13 +27,7 @@ public class SecurityConfig {
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
                 http
-                                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer
-                                                .configurationSource(corsConfigurationSource()))
-                                .csrf(AbstractHttpConfigurer::disable)
-                                .sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer
-                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                                .formLogin(AbstractHttpConfigurer::disable)
-                                .httpBasic(AbstractHttpConfigurer::disable)
+                                .csrf(csrf -> csrf.disable())
                                 .authorizeHttpRequests(auth -> auth
                                                 .requestMatchers(
                                                                 "/api/user/auth/login",
@@ -67,21 +54,5 @@ public class SecurityConfig {
                                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, redisService),
                                                 UsernamePasswordAuthenticationFilter.class);
                 return http.build();
-        }
-
-        @Bean
-        public CorsConfigurationSource corsConfigurationSource() {
-                CorsConfiguration config = new CorsConfiguration();
-
-                config.setAllowCredentials(true);
-                config.setAllowedOriginPatterns(List.of("*"));
-                config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-                config.setAllowedHeaders(List.of("*"));
-                config.setExposedHeaders(List.of("*"));
-
-                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                source.registerCorsConfiguration("/**", config);
-
-                return source;
         }
 }
